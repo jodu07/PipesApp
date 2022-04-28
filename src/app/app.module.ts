4095d37c2c13492f0314c11d59cafe1f023963d5
jodu07 <jhonnat.dussan@gmail.com>
2022-04-28 01:29:29 -0500

    items menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeNgModule } from './prime-ng/prime-ng.module';
 import { SharedModule } from './shared/shared.module';
-import { VentasModule } from './ventas/ventas.module';
+import { VentasModule } from './ventas/ventas.module'
+
 
 @NgModule({
   declarations: [
@@ -16,7 +16,7 @@ import { VentasModule } from './ventas/ventas.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    VentasModule  
+    VentasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -17,40 +17,29 @@ export class MenuComponent implements OnInit {
   ngOnInit(): void {
     this.items = [
       {
-          label:'File',
-          icon:'pi pi-fw pi-file',
+          label:'Pipes de Angular',
+          icon:'pi pi-desktop',
           items:[
               {
-                  label:'New',
-                  icon:'pi pi-fw pi-plus',
-                  items:[
-                  {
-                      label:'Bookmark',
-                      icon:'pi pi-fw pi-bookmark'
-                  },
-                  {
-                      label:'Video',
-                      icon:'pi pi-fw pi-video'
-                  },
-
-                  ]
+                  label:'Textos y fechas',
+                  icon:'pi pi-align-left',
+                  routerLink: '/'                  
               },
               {
-                  label:'Delete',
-                  icon:'pi pi-fw pi-trash'
-              },
+                  label:'Números',
+                  icon:'pi pi-dollar',
+                  routerLink: 'numeros'  
+              },              
               {
-                  separator:true
-              },
-              {
-                  label:'Export',
-                  icon:'pi pi-fw pi-external-link'
+                  label:'No comunes',
+                  icon:'pi pi-globe',
+                  routerLink: 'noComunes'
               }
           ]
       },
       {
-          label:'Edit',
-          icon:'pi pi-fw pi-pencil',
+          label:'Pipes personalizados',
+          icon:'pi pi-cog',
           items:[
               {
                   label:'Left',
@@ -70,78 +59,7 @@ export class MenuComponent implements OnInit {
               },
 
           ]
-      },
-      {
-          label:'Users',
-          icon:'pi pi-fw pi-user',
-          items:[
-              {
-                  label:'New',
-                  icon:'pi pi-fw pi-user-plus',
-
-              },
-              {
-                  label:'Delete',
-                  icon:'pi pi-fw pi-user-minus',
-
-              },
-              {
-                  label:'Search',
-                  icon:'pi pi-fw pi-users',
-                  items:[
-                  {
-                      label:'Filter',
-                      icon:'pi pi-fw pi-filter',
-                      items:[
-                          {
-                              label:'Print',
-                              icon:'pi pi-fw pi-print'
-                          }
-                      ]
-                  },
-                  {
-                      icon:'pi pi-fw pi-bars',
-                      label:'List'
-                  }
-                  ]
-              }
-          ]
-      },
-      {
-          label:'Events',
-          icon:'pi pi-fw pi-calendar',
-          items:[
-              {
-                  label:'Edit',
-                  icon:'pi pi-fw pi-pencil',
-                  items:[
-                  {
-                      label:'Save',
-                      icon:'pi pi-fw pi-calendar-plus'
-                  },
-                  {
-                      label:'Delete',
-                      icon:'pi pi-fw pi-calendar-minus'
-                  },
-
-                  ]
-              },
-              {
-                  label:'Archieve',
-                  icon:'pi pi-fw pi-calendar-times',
-                  items:[
-                  {
-                      label:'Remove',
-                      icon:'pi pi-fw pi-calendar-minus'
-                  }
-                  ]
-              }
-          ]
-      },
-      {
-          label:'Quit',
-          icon:'pi pi-fw pi-power-off'
-      }
+      }     
   ];
 }
   
