22896019210fbacd196a58e38fab8bc8cddfe28d
jodu07 <jhonnat.dussan@gmail.com>
2022-04-28 02:15:53 -0500

    pipes text basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PrimeNgModule } from './prime-ng/prime-ng.module';
 import { SharedModule } from './shared/shared.module';
-import { VentasModule } from './ventas/ventas.module'
+import { VentasModule } from './ventas/ventas.module';
+import { AppRoutingModule } from './app-routing.module';
+
+
+import { AppComponent } from './app.component';
+
 
 
 @NgModule({
