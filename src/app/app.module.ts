1654628546b8e2f7eb4cc4e3b1749edf18652cfb
jodu07 <jhonnat.dussan@gmail.com>
2022-04-28 18:30:41 -0500

    change languaje global app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
@@ -7,6 +7,10 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+// cambiar el locale de la app -- cambiar idioma en toda la aplición 
+import localeEs from '@angular/common/locales/es-CO';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
 
 
 @NgModule({
@@ -19,7 +23,9 @@ import { AppComponent } from './app.component';
     SharedModule,
     VentasModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-CO' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
