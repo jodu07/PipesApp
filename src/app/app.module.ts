9772c73ff4f9043fbcdffdecb1564b8984f87aa4
jodu07 <jhonnat.dussan@gmail.com>
2022-04-28 19:01:19 -0500

    date format a languajes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
@@ -8,9 +10,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 // cambiar el locale de la app -- cambiar idioma en toda la aplición 
+// importar idioma
 import localeEs from '@angular/common/locales/es-CO';
+import localeFr from '@angular/common/locales/fr';
+
+// importar funcion para registrar idioma
 import { registerLocaleData } from '@angular/common';
+
+//registrar idioma en la funcion
 registerLocaleData(localeEs);
+registerLocaleData(localeFr);
 
 
 @NgModule({
@@ -24,7 +33,9 @@ registerLocaleData(localeEs);
     VentasModule
   ],
   providers: [
-    { provide: LOCALE_ID, useValue: 'es-CO' }
+
+    // aplicar idioma desde la raiz en la app
+    { provide: LOCALE_ID, useValue: 'es-CO'}
   ],
   bootstrap: [AppComponent]
 })
