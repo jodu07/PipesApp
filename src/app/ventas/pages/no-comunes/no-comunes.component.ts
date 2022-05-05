90db2d71aacc06588fddfe3bc6048869b1b28b46
jodu07 <jhonnat.dussan@gmail.com>
2022-05-05 00:58:45 -0500

    tarea cambiar persona y borrar cliente con pipes 18nSelect y 18nPlural

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Persona {
+  nombre: string;
+  genero: string;
+
+}
+
 
 @Component({
   selector: 'app-no-comunes',
@@ -10,23 +16,69 @@ import { Component, OnInit } from '@angular/core';
 export class NoComunesComponent {
 
 //select
+
+personas: Persona[] = [
+  {
+    nombre: 'Jhonnatan',
+    genero: 'Masculino' 
+  },
+  {
+    nombre: 'Claudia',
+    genero: 'Femenino' 
+  },
+  {
+    nombre: 'Angeline',
+    genero: 'Femenino' 
+  },
+  {
+    nombre: 'Rocio',
+    genero: 'Femenino' 
+  },
+  {
+    nombre: 'Eduar',
+    genero: 'Masculino' 
+  },
+  {
+    nombre: 'Falcao',
+    genero: 'Masculino' 
+  },
+  
+]
+
 nombre: string = 'Claudia';
 genero: string = 'femenino';
-
 invitacionMapa = {
-  'masculino': 'invitarlo',
-  'femenino': 'invitarla'
+  'Masculino': 'invitarlo',
+  'Femenino': 'invitarla'
 }
 
 //plural
-clientes: string[] = ['Maria', 'Maria', 'Maria', 'Maria', 'Maria', 'Maria'];
+clientes: string[] = ['Elena', 'Maria', 'Maria', 'Maria', 'Maria', 'Maria'];
 clientesMapa = {
   '=0': 'no tenemos ningun cliente esperando',
   '=1': 'tenemos un cliente esperando',
   '=2': 'tenemos dos clientes esperando',
-  'other': 'tenemos # clientes esperando'
+  'other': 'tenemos # clientes esperando' 
+}
+
+
+cambiarCliente(){
+
+  const personaRandom = Math.floor(Math.random()*this.personas.length);
+
+ //console.log(this.personas[personaRandom].nombre);   
+
+ this.nombre = this.personas[personaRandom].nombre;
+
+ this.genero = this.personas[personaRandom].genero;
+
+ // console.log(personaRandom);
   
-  
+}
+
+borrarCliente(){
+  this.clientes.splice(0,1);
+  console.log('borr', this.clientes);
 }
 
 
