3adf4976d5307f5511490b1640df4ff560f566fe
jodu07 <jhonnat.dussan@gmail.com>
2022-05-06 01:03:24 -0500

    table data

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { Color, Heroe } from '../../interfaces/ventas.interface';
 
 @Component({
   selector: 'app-ordenar',
@@ -11,6 +12,33 @@ export class OrdenarComponent {
 
   enMayusculas: boolean=true;
 
+  heroe: Heroe[]=[
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Spiderman',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Aquaman',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ]
   
   cambiarATrue(){
     this.enMayusculas = !this.enMayusculas;
